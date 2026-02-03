aa60be03650e43fbd7f7765673a4606406d5d324
Vladyslav <specifically.for.programming@gmail.com>
2026-02-03 15:40:08 +0100

    deps init and ticket schemas fix

--- src/api/deps.py
+++ src/api/deps.py
@@ -1,7 +1,65 @@
-from fastapi import Depends
+from src.core.logger import logger
+
+import jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from pydantic import ValidationError
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.sql.annotation import Annotated
 
+from src.core import settings
 from src.db.session import get_db
+from src.models import User
+from src.schemas.token import TokenPayload
 
-DBDep = Annotated[AsyncSession, Depends(get_db())]
+reusable_oauth2 = OAuth2PasswordBearer(
+    tokenUrl=f'{settings.API_V1_STR}/auth/login'
+)
+
+DBDep = Annotated[AsyncSession, Depends(get_db)]
+
+async def get_current_user(
+        token: Annotated[str, Depends(reusable_oauth2)],
+        session: DBDep
+) -> User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='Could not validate credentials',
+        headers={'WWW-Authenticate': 'Bearer'}
+    )
+
+    try:
+        jwt_payload = jwt.decode(
+            token,
+            settings.SECRET_KEY,
+            algorithms = [settings.ALGORITHM]
+        )
+
+        token_data = TokenPayload(**jwt_payload)
+        if not token_data.sub:
+            logger.info('Auth failed: Token has no "sub" field')
+            raise credentials_exception
+
+    except jwt.ExpiredSignatureError:
+        logger.info('Auth failed: Toking expired')
+        raise credentials_exception
+    except (jwt.PyJWTError, ValidationError) as e:
+        logger.warning(f'Auth failed: Decode error : {e}')
+        raise credentials_exception
+
+    try:
+        user_id = int(token_data.sub)
+    except ValueError:
+        logger.warning(f'Auth failed: User Id is not an int: {token_data.sub}')
+        raise credentials_exception
+
+    user_query = select(User).where(User.id == user_id)
+    user_result = await session.execute(user_query)
+    user = user_result.scalar_one_or_none()
+
+    if not user:
+        logger.warning(f'Auth failed: User {user_id} not found in DB')
+        raise credentials_exception
+
+    return user
--- src/schemas/token.py
+++ src/schemas/token.py
@@ -1,5 +1,10 @@
-from pydantic import BaseModel
+from typing import Optional
+
+from pydantic import BaseModel, Field
 
 class Token(BaseModel):
     access_token: str
     token_type: str
+
+class TokenPayload(BaseModel):
+    sub: Optional[str] = Field(None, description='User Id')
