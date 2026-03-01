a892831ed3f3990bd2b98fbb4a1fbafe7e46391b
Vladyslav <specifically.for.programming@gmail.com>
2026-03-02 00:33:25 +0100

    ticket type schema implementation event and ticket schemas migration & refactoring (base model implementation)

---
+++ src/schemas/ticket_type.py
@@ -0,0 +1,42 @@
+from pydantic import BaseModel, ConfigDict, Field
+
+
+class TicketTypeBase(BaseModel):
+    name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        description="Ticket type name",
+        examples=["VIP", "Standard"],
+    )
+    price: int = Field(..., ge=0, description="Price in cents", examples=[10000])
+
+    tickets_quantity: int = Field(
+        ..., gt=0, description="Quantity of tickets of this type"
+    )
+
+
+class TicketTypeCreate(TicketTypeBase):
+    event_id: int = Field(..., description="Event id")
+
+
+class TicketTypeResponse(TicketTypeCreate):
+    id: int
+    tickets_sold: int
+    model_config = ConfigDict(from_attributes=True)
+
+
+class TicketTypeUpdate(BaseModel):
+    name: str | None = Field(
+        None,
+        min_length=1,
+        max_length=50,
+        description="Ticket type name",
+        examples=["VIP", "Standard"],
+    )
+    price: int | None = Field(
+        None, ge=0, description="Price in cents", examples=[10000]
+    )
+    tickets_quantity: int | None = Field(
+        None, gt=0, description="Quantity of tickets of this type"
+    )
