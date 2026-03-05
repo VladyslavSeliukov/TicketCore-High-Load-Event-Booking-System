28acd50560657683adbfdb93a84f9636ff2b9b78
Vladyslav <specifically.for.programming@gmail.com>
2026-03-05 03:07:50 +0100

    Ticket schema PositiveInt fix

--- src/schemas/ticket.py
+++ src/schemas/ticket.py
@@ -1,8 +1,8 @@
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict, Field, PositiveInt
 
 
 class TicketBase(BaseModel):
-    ticket_type_id: int = Field(..., description="Ticket type Id")
+    ticket_type_id: PositiveInt = Field(..., description="Ticket type Id")
 
 
 class TicketCreate(TicketBase):
@@ -15,6 +15,7 @@ class TicketResponse(TicketBase):
 
     model_config = ConfigDict(from_attributes=True)
 
+    id: PositiveInt
 
 class TicketUpdate(BaseModel):
     ticket_type_id: int | None = Field(None, description="Ticket type Id")
